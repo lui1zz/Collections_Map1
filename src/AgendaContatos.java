0a97f6a1d6fb4441f10117e7d691432f87f295ec
lui1zz <luiz0921341@gmail.com>
2025-03-07 17:17:28 -0300

    Basico MAP

---
+++ src/AgendaContatos.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class AgendaContatos {
+
+    private Map<String, Integer> agendaContatosMap;
+
+    public AgendaContatos() {
+        this.agendaContatosMap = new HashMap<>();
+    }
+
+    public void adicionarContato(String nome, Integer telefone) {
+        agendaContatosMap.put(nome, telefone);
+    }
+
+    public void removerContato(String nome) {
+        if(!agendaContatosMap.isEmpty()){
+            agendaContatosMap.remove(nome);
+        }
+    }
+
+    public void exibirContatos(){
+        System.out.println(agendaContatosMap);
+    }
+
+    public Integer pesquisarPorNome(String nome) {
+        Integer numeroPorNome= null;
+        if(!agendaContatosMap.isEmpty()) {
+            agendaContatosMap.get(nome);
+        }
+        return numeroPorNome;
+    }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.exibirContatos();
+        agendaContatos.adicionarContato("Lu", 213213);
+        agendaContatos.exibirContatos();
+        agendaContatos.removerContato("Lu");
+        agendaContatos.exibirContatos();
+    }
+}
