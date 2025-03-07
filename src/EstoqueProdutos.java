3adc75e7ea4867e0dbebd3b67d3d58612c64da96
lui1zz <luiz0921341@gmail.com>
2025-03-07 17:38:07 -0300

    Estoque Produtos
    Signed-off-by: lui1zz <luiz0921341@gmail.com>

--- src/EstoqueProdutos.java
+++ src/EstoqueProdutos.java
@@ -1,2 +1,41 @@
+import java.util.Map;
+import java.util.HashMap;
 public class EstoqueProdutos {
+
+    private Map<Long, Produto> estoqueProdutosMap;
+
+    public EstoqueProdutos() {
+        this.estoqueProdutosMap = new HashMap<>();
+    }
+
+    public void adicionarPoduto(long cod, String nome, int quantidade, double preco) {
+        estoqueProdutosMap.put(cod, new Produto(nome, preco, quantidade));
+    }
+
+    public void exibirProdutos(){
+        System.out.println(estoqueProdutosMap);
+    }
+
+    public double calcularValorTotalEstoque() {
+        double valorTotalEstoque = 0d;
+        if (estoqueProdutosMap.isEmpty()) {
+            for(Produto p: estoqueProdutosMap.values()) {
+                valorTotalEstoque += p.getQuantidade() * p.getPreco();
+            }
+        }
+        return valorTotalEstoque;
+    }
+
+    public Produto ProdutoMaisCaro(){
+        Produto produtoMaiasCaro = null;
+        double maiorPreco = Double.MIN_VALUE;
+        if (estoqueProdutosMap.isEmpty()) {
+            for (Produto p : estoqueProdutosMap.values()) {
+                if(p.getPreco() > maiorPreco) {
+                    produtoMaiasCaro = p;
+                }
+            }
+        }
+        return ProdutoMaisCaro();
+    }
 }
