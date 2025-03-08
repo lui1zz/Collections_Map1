eb2b776438f9002273539f2101b0e7275248c7ff
lui1zz <luiz0921341@gmail.com>
2025-03-07 23:51:40 -0300

    AgendaEventos pt.2

--- src/AgendaEventos.java
+++ src/AgendaEventos.java
@@ -22,6 +22,21 @@ public class AgendaEventos {
         System.out.println(eventosTreeMap);
     }
 
+    public void obterProximoEvento() {
+        LocalDate dataAtual = LocalDate.now();
+        LocalDate proximaData = null;
+        Evento proximoEvento = null;
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        for(Map.Entry<LocalDate, Evento> entry: eventosTreeMap.entrySet()) {
+                System.out.println(entry.getValue());
+            if(entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)) {
+                proximaData = entry.getKey();
+                proximoEvento= entry.getValue();
+                System.out.println(entry.getValue() + " on date: " + entry.getKey());
+            }
+        }
+    }
+
     public static void main(String[] args) {
        AgendaEventos agendaEventos = new AgendaEventos();
     }
